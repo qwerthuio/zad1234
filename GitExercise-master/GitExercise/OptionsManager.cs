48f12358769667dcf2f9c74d218d8a1a0077b6da
barnabans <116289188+barnabans@users.noreply.github.com>
2022-11-16 14:30:53 +0200

    69

---
+++ GitExercise-master/GitExercise/OptionsManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace GitExercise
+{
+    public static class OptionsManager
+    {
+        public static string[] OptionsList = {
+            "a - Add",
+            "s - Subtract",
+            "m - Multiply",
+            "pow - Power",
+            "log - Logarithm",
+            "fact - sum of 2 factorials "
+        };
+
+        public static void Add(double a, double b)
+        {
+            Console.WriteLine($"{a} + {b} = {a + b}");
+        }
+
+        public static void Multiply(double a, double b)
+        {
+            Console.WriteLine($"{a} x {b} = {a * b}");
+        }
+
+        public static void Subtract(double a, double b)
+        {
+            Console.WriteLine($"{a} - {b} = {a - b}");
+        }
+
+        internal static void Power(double a, double b)
+        {
+            Console.WriteLine($"{a} ^ {b} = {Math.Pow(a,b)}");
+        }
+
+        internal static void Log(double a, double b)
+        {
+            Console.WriteLine($"log of {a} with base {b} = {Math.Log(a - b)}");
+        }
+
+        internal static void Factoria(double a, double b)
+        {
+            long factA = CalculateFact((int)a);
+            long factB = CalculateFact((int)b);
+            Console.WriteLine($"{a}! + {b}! = {(int)a}! + {(int)b}! = {factA + factB}");
+
+        }
+        private static long CalculateFact(int a)
+        {
+            long result = a;
+            for (int i = a - 1; i >= 1; i--)
+            {
+                result = result *i;
+            }
+            return result;
+        }
+    }
+}
